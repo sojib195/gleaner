ab05142faa16056c7619cd586c397d8e37b8ad41
adplincinst <apadilla@lincolninst.edu>
2024-05-29 05:25:06 +0000

    added non-zero exit code on assert script fail

--- scripts/iow/assert-s3-dir-cnt.sh
+++ scripts/iow/assert-s3-dir-cnt.sh
@@ -14,6 +14,6 @@ ACTUAL_COUNT=`$MC ls $DIR| wc -l`
 
 if [ "$DESIRED_COUNT" -ne "$ACTUAL_COUNT" ]
 then
-     echo "$DIR: wanted $DESIRED_COUNT directory entries, found $ACTUAL_COUNT"
+     echo "$DIR: wanted $DESIRED_COUNT directory entries, found $ACTUAL_COUNT" && exit 1
 fi
 
