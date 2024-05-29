a24c9b9728d3cf8c1cccfbb181d7f971fc283dc9
adplincinst <apadilla@lincolninst.edu>
2024-05-29 04:42:02 +0000

    gh actions #8

--- scripts/iow/start-gleaner.sh
+++ scripts/iow/start-gleaner.sh
@@ -3,20 +3,13 @@
 
 
 
-TS=`date +%Y-%m-%dT%H.%M.%S`
-LOGDIR="$HOME/logs/$TS"
-mkdir -p $LOGDIR || exit 1
-cd $LOGDIR || exit 1
 
-for src in `cat ~/conf/gleanerconfig.yaml | grep '\Wname:'|awk '{print $2}'`
+for src in `cat configs/iow/pids-geoconnex-dev-gleanerconfig.yaml | grep '\Wname:'|awk '{print $2}'`
 do
 
-OUTFILE="$LOGDIR/gleaner-$src.out"
-ERRFILE="$LOGDIR/gleaner-$src.err"
 
 echo "harvesting source '$src'..."
-#strace -f -o $LOGDIR/strace-$src.out gleaner -cfg $HOME/conf/gleanerconfig.yaml -source $src -rude > $OUTFILE 2>$ERRFILE
-gleaner -log debug  -cfg $HOME/conf/gleanerconfig.yaml -source $src -rude > $OUTFILE 2>$ERRFILE
+gleaner -log debug  -cfg config/gleanerconfig.yaml -source $src -rude
 done
 echo "complete!"
 
